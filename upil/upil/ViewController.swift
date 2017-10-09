c514593df11ae1e8e15b4ac29430175a456cfd22
Upilup123 <32662178+Upilup123@users.noreply.github.com>
2017-10-10 04:39:35 +0700

    new upil

---
+++ upil/upil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  upil
+//
+//  Created by Luthfi Aly on 10/10/17.
+//  Copyright © 2017 Luthfialy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
